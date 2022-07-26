0d4bdc557880d2373ab2ff5e85ba64c54b28bb36
yogesh-grl <yogesh@graniteriverlabs.in>
2022-07-26 17:16:33 +0530

    Sample Project

---
+++ BlazorSample/Pages/Movies/Movies.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorSample.Pages.Movies
+{
+    public class Movies
+    {
+        public int dMovieID = 0;
+        public string strMovieName = "";
+        public string strMovieRelDate = "";
+        public string strMovieLabel = "";
+
+        public Movies(int dMovieID, string strMovieName, string strMovieRelDate, string strMovieLabel)
+        {
+            this.dMovieID = dMovieID;
+            this.strMovieName = strMovieName;
+            this.strMovieRelDate = strMovieRelDate;
+            this.strMovieLabel = strMovieLabel;
+        }
+    }
+}
